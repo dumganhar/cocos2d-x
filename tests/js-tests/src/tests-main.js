2528acea177c4ae5f11250d403871e91699901e1
James Chen <jianhua.chen@cocos2d-x.org>
2017-06-07 13:49:28 +0800

    reverts js tests main

--- tests/js-tests/src/tests-main.js
+++ tests/js-tests/src/tests-main.js
@@ -256,47 +256,47 @@ var testNames = [
             return new ActionsTestScene();
         }
     },
-//cjh    {
-//        title:"Bake Layer Test",
-//        platforms: PLATFORM_HTML5,
-//        linksrc:"src/BakeLayerTest/BakeLayerTest.js",
-//        testScene:function () {
-//            return new BakeLayerTestScene();
-//        }
-//    },
-//    {
-//        title:"BillBoard Test",
-//        platforms: PLATFORM_JSB,
-//        linksrc:"src/BillBoardTest/BillBoardTest.js",
-//        testScene:function () {
-//            return new BillBoardTestScene();
-//        }
-//    },
-//    {
-//        title:"Box2D Test",
-//        resource:g_box2d,
-//        platforms: PLATFORM_HTML5,
-//        linksrc:"src/Box2dTest/Box2dTest.js",
-//        testScene:function () {
-//            return new Box2DTestScene();
-//        }
-//    },
-//    {
-//        title:"Camera3D Test",
-//        platforms: PLATFORM_JSB,
-//        linksrc:"src/Camera3DTest/Camera3DTest.js",
-//        testScene:function () {
-//            return new Camera3DTestScene();
-//        }
-//    },
-//    {
-//        title:"Chipmunk Test",
-//        platforms: PLATFORM_ALL,
-//        linksrc:"src/ChipmunkTest/ChipmunkTest.js",
-//        testScene:function () {
-//            return new ChipmunkTestScene();
-//        }
-//    },
+    {
+       title:"Bake Layer Test",
+       platforms: PLATFORM_HTML5,
+       linksrc:"src/BakeLayerTest/BakeLayerTest.js",
+       testScene:function () {
+           return new BakeLayerTestScene();
+       }
+   },
+   {
+       title:"BillBoard Test",
+       platforms: PLATFORM_JSB,
+       linksrc:"src/BillBoardTest/BillBoardTest.js",
+       testScene:function () {
+           return new BillBoardTestScene();
+       }
+   },
+   {
+       title:"Box2D Test",
+       resource:g_box2d,
+       platforms: PLATFORM_HTML5,
+       linksrc:"src/Box2dTest/Box2dTest.js",
+       testScene:function () {
+           return new Box2DTestScene();
+       }
+   },
+   {
+       title:"Camera3D Test",
+       platforms: PLATFORM_JSB,
+       linksrc:"src/Camera3DTest/Camera3DTest.js",
+       testScene:function () {
+           return new Camera3DTestScene();
+       }
+   },
+   {
+       title:"Chipmunk Test",
+       platforms: PLATFORM_ALL,
+       linksrc:"src/ChipmunkTest/ChipmunkTest.js",
+       testScene:function () {
+           return new ChipmunkTestScene();
+       }
+   },
     {
         title:"ClippingNode Test",
         platforms: PLATFORM_ALL,
@@ -397,14 +397,14 @@ var testNames = [
             return new NativeTestScene();
         }
     },
-    //{
-    //    title:"Facebook SDK Test",
-    //    platforms: PLATFROM_ANDROID | PLATFROM_IOS | PLATFORM_HTML5,
-    //    linksrc:"src/FacebookTest/FacebookTestsManager.js",
-    //    testScene:function () {
-    //        return new FacebookTestScene();
-    //    }
-    //},
+    {
+       title:"Facebook SDK Test",
+       platforms: PLATFROM_ANDROID | PLATFROM_IOS | PLATFORM_HTML5,
+       linksrc:"src/FacebookTest/FacebookTestsManager.js",
+       testScene:function () {
+           return new FacebookTestScene();
+       }
+    },
     {
         title:"Font Test",
         resource:g_fonts,
@@ -449,14 +449,14 @@ var testNames = [
             return new LayerTestScene();
         }
     },
-//cjh    {
-//        title:"Light Test",
-//        platforms: PLATFORM_JSB,
-//        linksrc:"src/LightTest/LightTest.js",
-//        testScene:function () {
-//            return new LightTestScene();
-//        }
-//    },
+    {
+       title:"Light Test",
+       platforms: PLATFORM_JSB,
+       linksrc:"src/LightTest/LightTest.js",
+       testScene:function () {
+           return new LightTestScene();
+       }
+   },
     {
         title:"Loader Test",
         platforms: PLATFORM_ALL,
@@ -465,23 +465,23 @@ var testNames = [
             return new LoaderTestScene();
         }
     },
-//cjh    {
-//        title:"MaterialSystem Test",
-//        platforms: PLATFORM_JSB,
-//        linksrc:"src/MaterialSystemTest/MaterialSystemTest.js",
-//        testScene:function () {
-//            return new MaterialSystemTestScene();
-//        }
-//    },
-//    {
-//        title:"Memory Model Test",
-//        resource:g_menu,
-//        platforms: PLATFORM_JSB,
-//        linksrc:"src/MemoryModelTest/MemoryModelTest.js",
-//        testScene:function () {
-//            return new MemoryModelTestScene();
-//        }
-//    },
+    {
+       title:"MaterialSystem Test",
+       platforms: PLATFORM_JSB,
+       linksrc:"src/MaterialSystemTest/MaterialSystemTest.js",
+       testScene:function () {
+           return new MaterialSystemTestScene();
+       }
+   },
+   {
+       title:"Memory Model Test",
+       resource:g_menu,
+       platforms: PLATFORM_JSB,
+       linksrc:"src/MemoryModelTest/MemoryModelTest.js",
+       testScene:function () {
+           return new MemoryModelTestScene();
+       }
+   },
     {
         title:"Menu Test",
         resource:g_menu,
@@ -507,15 +507,15 @@ var testNames = [
             return new NodeTestScene();
         }
     },
-//cjh    {
-//        title:"OpenGL Test",
-//        resource:g_opengl_resources,
-//        platforms: PLATFORM_JSB_AND_WEBGL,
-//        linksrc:"src/OpenGLTest/OpenGLTest.js",
-//        testScene:function () {
-//            return new OpenGLTestScene();
-//        }
-//    },
+    {
+       title:"OpenGL Test",
+       resource:g_opengl_resources,
+       platforms: PLATFORM_JSB_AND_WEBGL,
+       linksrc:"src/OpenGLTest/OpenGLTest.js",
+       testScene:function () {
+           return new OpenGLTestScene();
+       }
+   },
     {
         title:"Parallax Test",
         resource:g_parallax,
@@ -525,13 +525,13 @@ var testNames = [
             return new ParallaxTestScene();
         }
     },
-//cjh    {
-//        title:"Particle3D Test",
-//        platforms: PLATFORM_JSB,
-//        testScene:function () {
-//            return new Particle3DTestScene();
-//        }
-//    },
+    {
+       title:"Particle3D Test",
+       platforms: PLATFORM_JSB,
+       testScene:function () {
+           return new Particle3DTestScene();
+       }
+   },
     {
         title:"Particle Test",
         platforms: PLATFORM_ALL,
@@ -557,22 +557,22 @@ var testNames = [
             return new PerformanceNowTestScene();
         }
     },
-//cjh    {
-//        title:"Physics3D Test",
-//        platforms: PLATFORM_JSB,
-//        linksrc:"src/Physics3DTest/Physics3DTest.js",
-//        testScene:function () {
-//            return new Physics3DTestScene();
-//        }
-//    },
-//    {
-//        title:"NavMesh Test",
-//        platforms: PLATFORM_JSB,
-//        linksrc:"src/NavMeshTest/NavMeshTest.js",
-//        testScene:function () {
-//            return new nextNavMeshTest();
-//        }
-//    },
+    {
+       title:"Physics3D Test",
+       platforms: PLATFORM_JSB,
+       linksrc:"src/Physics3DTest/Physics3DTest.js",
+       testScene:function () {
+           return new Physics3DTestScene();
+       }
+   },
+   {
+       title:"NavMesh Test",
+       platforms: PLATFORM_JSB,
+       linksrc:"src/NavMeshTest/NavMeshTest.js",
+       testScene:function () {
+           return new nextNavMeshTest();
+       }
+   },
     {
         title:"ProgressActions Test",
         platforms: PLATFORM_ALL,
@@ -621,31 +621,31 @@ var testNames = [
             return new SchedulerTestScene();
         }
     },
-//cjh    {
-//        title:"Spine Test",
-//        resource: g_spine,
-//        platforms: PLATFORM_ALL,
-//        linksrc:"src/SpineTest/SpineTest.js",
-//        testScene:function () {
-//            return new SpineTestScene();
-//        }
-//    },
-//    {
-//        title:"Sprite3D Test",
-//        platforms: PLATFORM_JSB,
-//        linksrc:"src/Sprite3DTest/Sprite3DTest.js",
-//        testScene:function () {
-//            return new Sprite3DTestScene();
-//        }
-//    },
-//    {
-//        title:"SpritePolygon Test",
-//        platforms: PLATFORM_JSB,
-//        linksrc:"src/SpritePolygonTest/SpritePolygonTest.js",
-//        testScene:function () {
-//            return new SpritePolygonTestScene();
-//        }
-//    },
+    {
+       title:"Spine Test",
+       resource: g_spine,
+       platforms: PLATFORM_ALL,
+       linksrc:"src/SpineTest/SpineTest.js",
+       testScene:function () {
+           return new SpineTestScene();
+       }
+   },
+   {
+       title:"Sprite3D Test",
+       platforms: PLATFORM_JSB,
+       linksrc:"src/Sprite3DTest/Sprite3DTest.js",
+       testScene:function () {
+           return new Sprite3DTestScene();
+       }
+   },
+   {
+       title:"SpritePolygon Test",
+       platforms: PLATFORM_JSB,
+       linksrc:"src/SpritePolygonTest/SpritePolygonTest.js",
+       testScene:function () {
+           return new SpritePolygonTestScene();
+       }
+   },
     {
         title:"Sprite Test",
         resource:g_sprites,
@@ -664,14 +664,14 @@ var testNames = [
             return new S9SpriteTestScene();
         }
     },
-//cjh    {
-//        title:"Terrain Test",
-//        platforms: PLATFORM_JSB,
-//        linksrc:"src/TerrainTest/TerrainTest.js",
-//        testScene:function () {
-//            return new TerrainTestScene();
-//        }
-//    },
+    {
+       title:"Terrain Test",
+       platforms: PLATFORM_JSB,
+       linksrc:"src/TerrainTest/TerrainTest.js",
+       testScene:function () {
+           return new TerrainTestScene();
+       }
+   },
     {
         title:"TextInput Test",
         platforms: PLATFORM_HTML5,
